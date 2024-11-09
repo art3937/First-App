0e3996320c14dc90134ab0ce9a2bac6c02591453
Artiom test <art3937@gmail.com>
2024-11-09 19:12:07 +0300

    varAndVal

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
 fun main(){
     println("First App")
+    val itemPrice = 100
+    val itemCount = 11
+    val discount = 100
+    val discountStart = 1_0000
+
+    var totalPrise = itemPrice * itemCount
+    if(totalPrise > discountStart){
+        totalPrise -= discount
+    }
+    println(totalPrise)
 }
